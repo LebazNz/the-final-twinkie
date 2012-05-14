295d662829ba89e2a42923e0a9b2ae512ff56937
lebaz92@yahoo.com <lebaz92@yahoo.com@9ebc302b-8130-08b0-9958-765a603d3a29>
2012-05-14 17:17:09 +0000

    Fixed Particle Effects

--- FinalTwinkie/FinalTwinkie/source/Particle.cpp
+++ FinalTwinkie/FinalTwinkie/source/Particle.cpp
@@ -9,13 +9,13 @@ void CParticle::Render(void)
 {
 }
 
-void CParticle::CreateParticle(float fLife, float fVelocityX, float fVelocityY, float fScale, int nPosX, int nPosY, DWORD dwColor)
+void CParticle::CreateParticle(float fLife, float fVelocityX, float fVelocityY, float fScale, float nPosX, float nPosY, DWORD dwColor)
 {
 	m_fCurLife=fLife;
 	m_fCurVelocityX=fVelocityX;
 	m_fCurVelocityY=fVelocityY;
 	m_fScale=fScale;
-	m_nCurPosX=nPosX;
-	m_nCurPosY=nPosY;
+	m_fCurPosX=nPosX;
+	m_fCurPosY=nPosY;
 	m_dwColor=dwColor;
 }
--- FinalTwinkie/FinalTwinkie/source/Particle.h
+++ FinalTwinkie/FinalTwinkie/source/Particle.h
@@ -8,15 +8,15 @@ class CParticle
 public:
 	void Update(float fDt);
 	void Render(void);
-	void CreateParticle(float fLife, float fVelocityX, float fVelocityY, float fScale, int nPosX, int nPosY, DWORD dwColor);
+	void CreateParticle(float fLife, float fVelocityX, float fVelocityY, float fScale, float nPosX, float nPosY, DWORD dwColor);
 
 //private:
 	float m_fCurLife;
 	float m_fCurVelocityX;
 	float m_fCurVelocityY;
 	float m_fScale;
-	float m_nCurPosX;
-	float m_nCurPosY;
+	float m_fCurPosX;
+	float m_fCurPosY;
 	DWORD m_dwColor;
 
 };
